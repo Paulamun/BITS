8f98425fd68479da2b81e209638b467c3b6e9e6b
Paulamun <130382812+Paulamun@users.noreply.github.com>
2024-06-06 20:20:04 -0700

    Update Step_3_Con_files_to_txt.sh

--- Step_3_Con_files_to_txt.sh
+++ Step_3_Con_files_to_txt.sh
@@ -1,18 +1,49 @@
 #FOR HC GROUP BASELINE 
-
-for sample in 022 023 044 045 048 051 060 
+for sample in 044 045 048 051 060 
 do
-3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/users/paulamun/230502_JP_Paula_GoNoGo_level1/Con_Files_GoNoGoTaskBaselineHD_new/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0001.nii > /oak/stanford/groups/leanew1/users/paulamun/230502_JP_Paula_GoNoGo_level1/txt_Files_GoNoGoTaskBaselineHD/Con_0001/sub-BITS"$sample".txt
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0001.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskBaselineHC/sub-BITS"$sample"_Con_0001.txt
 done
 
-for sample in 022 023 044 045 048 051 060 
+for sample in  044 045 048 051 060 
 do
-3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/users/paulamun/230502_JP_Paula_GoNoGo_level1/Con_Files_GoNoGoTaskBaselineHD_new/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0002.nii > /oak/stanford/groups/leanew1/users/paulamun/230502_JP_Paula_GoNoGo_level1/txt_Files_GoNoGoTaskBaselineHD/Con_0002/sub-BITS"$sample".txt
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0002.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskBaselineHC/sub-BITS"$sample"_Con_0002.txt
 done
 
-for sample in 022 023 044 045 048 051 060 
+for sample in 044 045 048 051 060 
 do
-3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/users/paulamun/230502_JP_Paula_GoNoGo_level1/Con_Files_GoNoGoTaskBaselineHD_new/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0003.nii > /oak/stanford/groups/leanew1/users/paulamun/230502_JP_Paula_GoNoGo_level1/txt_Files_GoNoGoTaskBaselineHD/Con_0003/sub-BITS"$sample".txt
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0003.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskBaselineHC/sub-BITS"$sample"_Con_0003.txt
 done
 
 
+#FOR HD GROUP BASELINE 
+for sample in 028 033 021 015 062 020 024 027 030 055 056 057 063 054 039 040 031 042 014 026
+do
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0001.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskBaselineHD/sub-BITS"$sample"_Con_0001.txt
+done
+
+for sample in  028 033 021 015 062 020 024 027 030 055 056 057 063 054 039 040 031 042 014 026
+do
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0002.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskBaselineHD/sub-BITS"$sample"_Con_0002.txt
+done
+
+for sample in 028 033 021 015 062 020 024 027 030 055 056 057 063 054 039 040 031 042 014 026
+do
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0003.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskBaselineHD/sub-BITS"$sample"_Con_0003.txt
+done
+
+
+#FOR HD GROUP WEEK 18
+for sample in 028 033 021 015 020 024 027 030 014 026
+do
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0001.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskWeek18HD/sub-BITS"$sample"_Con_0001.txt
+done
+
+for sample in  028 033 021 015 020 024 027 030 014 026
+do
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0002.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskWeek18HD/sub-BITS"$sample"_Con_0002.txt
+done
+
+for sample in 028 033 021 015 020 024 027 030 014 026
+do
+3dROIstats -mask /oak/stanford/groups/leanew1/BITS/PM_Mask.nii /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/sub-BITS"$sample"/ses-s0/gonogo/activation/all/con_0003.nii > /oak/stanford/groups/leanew1/SHARED_DATASETS/private/BITS/data/bids-input/bids-input/txt_Files_GoNoGoTaskWeek18HD/sub-BITS"$sample"_Con_0003.txt
+done
